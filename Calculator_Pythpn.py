4b7db8ebc509fc579b86e8702e55fbfef32e7e26
Rahima0512 <77523450+Rahima0512@users.noreply.github.com>
2021-06-03 00:22:33 +0530

    Update Calculator_Pythpn.py

--- Calculator_Pythpn.py
+++ Calculator_Pythpn.py
@@ -13,19 +13,31 @@ def div(n1,n2):
     return n1/n2
 
 Operations={'+':add,'-':sub,'*':mul,'/':div}
-continue1='y'
+
 def calculator_func():
+    continue1 = True
     num1 = float(input("Enter the first number\t"))
-    num2 = float(input("Enter the second number\t"))
+    while (continue1 == True):
+        for key in Operations:
+            print(key)
+        symbol = input("Enter the operation to be performed:\t")
+        num2 = float(input("Enter the second number\t"))
+        func_perform = Operations[symbol]
+        answer_1 = func_perform(num1, num2)
+        print(f"\t {num1} {symbol} {num2}\t=\t {answer_1}")
 
-    for key in Operations:
-        print(key)
-    symbol = input("Enter the operation to be performed:\t")
+        continue1 = input("Do you wanna continue? y/n or start a new calculation : new\t")
+        if continue1=='n':
+            exit()
+        if continue1=='new':
+            continue1=True
+            calculator_func()
+        if continue1=='y':
+            continue1 = True
+            num1=answer_1
+continue1=True
+calculator_func()
 
-    func_perform = Operations[symbol]
-    answer = func_perform(num1, num2)
-    print(f"\t {num1} {symbol} {num2}\t=\t {answer}")
-while(continue1=='y'):
-    calculator_func()
-    continue1=input("Do you wanna continue? y/n\t")
+
+
 
