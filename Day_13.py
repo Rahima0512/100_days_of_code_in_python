7ff5b6cc520f473ca8b5538e1e60fca450190b1b
Rahima0512 <77523450+Rahima0512@users.noreply.github.com>
2021-06-06 23:28:12 +0530

    Add files via upload

---
+++ Day_13.py
@@ -0,0 +1,13 @@
+from random import randint
+"""def my_func():
+    for i in range(1,21):
+        if i==20:
+            print("You got it")
+
+my_func()
+"""
+"""dice_imgs=[1,2,3,4,5,6]
+dice_num=randint(0,5)
+print(dice_imgs[dice_num])"""
+
+
